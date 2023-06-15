205dee987fe8524f1ff5f966c24aeb3748c5e9d3
Sergey Polyakov <PolyusIO@yandex.ru>
2023-06-15 16:47:33 +0500

    add model

--- ContactList/Models/Person.swift
+++ ContactList/Models/Person.swift
@@ -5,4 +5,15 @@
 //  Created by Сергей Поляков on 15.06.2023.
 //
 
-import Foundation
+//import Foundation
+
+struct Person {
+    let name: String
+    let surname: String
+    let email: String
+    let phone: String
+    
+    var fullName: String {
+        "\(name) \(surname)"
+    }
+}
